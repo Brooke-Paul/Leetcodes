1d7809a947b58a69e560fd4a8f492d51f22a45c2
xuetao <gentle.taoxue@gmail.com>
2019-04-25 23:36:08 +0800

    add leetcode5 palindrome

---
+++ src/main/java/com/learning/LeetCode5.java
@@ -0,0 +1,45 @@
+package com.learning;
+
+/**
+ * @Author xuetao
+ * @Description: 给定一个字符串s， 找到 s 中最长的回文子串
+ * 输入：'babad' 输出 'bab' 或者 'aba'
+ * 输入：'cbbd'  输出 'bb'
+ * @Date 2019-04-25
+ * @Version 1.0
+ */
+public class LeetCode5 {
+
+    private int lo, maxLen;
+
+    public static void main(String[] args) {
+        String str = "abaccab";
+        LeetCode5 leetCode5 = new LeetCode5();
+        System.out.println(leetCode5.longgestStr(str));
+    }
+
+
+    public String longgestStr(String s) {
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+        int len = s.length();
+
+        for (int i = 0; i < len - 1; i++) {
+            extendPalindrome(s, i, i);
+            extendPalindrome(s, i, i + 1);
+        }
+        return s.substring(lo, lo + maxLen);
+    }
+
+    private void extendPalindrome(String s, int j, int k) {
+        while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
+            j--;
+            k++;
+        }
+        if (maxLen < k - j - 1) {
+            lo = j + 1;
+            maxLen = k - j - 1;
+        }
+    }
+}
