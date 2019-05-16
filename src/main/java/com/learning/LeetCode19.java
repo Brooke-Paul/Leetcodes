793014738da201bc866afb2a14a14ce4ea7b9f96
xuetao <gentle.taoxue@gmail.com>
2019-05-16 22:44:53 +0800

    insert leetcode19 linked list delete

---
+++ src/main/java/com/learning/LeetCode19.java
@@ -0,0 +1,31 @@
+package com.learning;
+
+/**
+ * @Author xuetao
+ * @Description: 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+ *
+ * 示例：
+ *
+ * 给定一个链表: 1->2->3->4->5, 和 n = 2.
+ *
+ * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+ *
+ * 说明：
+ *
+ * 给定的 n 保证是有效的。
+ *
+ * 进阶：
+ *
+ * 你能尝试使用一趟扫描实现吗？
+ * @Date 2019-05-16
+ * @Version 1.0
+ */
+public class LeetCode19 {
+    /**
+     * 链表实现删除 （思考明日解决)
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
