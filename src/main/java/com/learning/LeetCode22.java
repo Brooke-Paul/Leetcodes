1e415d125b691a983765be747a74a9b820029aaa
xuetao <gentle.taoxue@gmail.com>
2019-05-23 23:48:44 +0800

    insert leetcode22 constructBrackets

---
+++ src/main/java/com/learning/LeetCode22.java
@@ -0,0 +1,40 @@
+package com.learning;
+
+import java.util.*;
+
+/**
+ * @Author xuetao
+ * @Description: 给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且 有效的 括号组合。
+ * <p>
+ * 例如，给出 n = 3，生成结果为：
+ * <p>
+ * [
+ * "((()))",
+ * "(()())",
+ * "(())()",
+ * "()(())",
+ * "()()()"
+ * ]
+ * @Date 2019-05-23
+ * @Version 1.0
+ */
+public class LeetCode22 {
+
+    public static void main(String[] args) {
+        List<String> strings = new ArrayList<>();
+        constructBrackets(strings, 3, 3, "");
+        strings.forEach(i -> System.out.println(i));
+    }
+
+    public static void constructBrackets(List<String> strings, int left, int right, String s) {
+        if (left > 0) {
+            constructBrackets(strings, left - 1, right, s + "(");
+        }
+        if (right > left) {
+            constructBrackets(strings, left, right - 1, s + ")");
+        }
+        if (right == 0) {
+            strings.add(s);
+        }
+    }
+}
