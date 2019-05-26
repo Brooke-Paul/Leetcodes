0dbcabf8f8400a78dc2e2b94636c71ce6fcc2a2a
xuetao <gentle.taoxue@gmail.com>
2019-05-26 22:49:45 +0800

    insert leetcode24 swap linked

---
+++ src/main/java/com/learning/LeetCode24.java
@@ -0,0 +1,24 @@
+package com.learning;
+
+/**
+ * @Author xuetao
+ * @Description: 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+ * <p>
+ * 示例:
+ * <p>
+ * 给定 1->2->3->4, 你应该返回 2->1->4->3.
+ * <p>
+ * 说明:
+ * <p>
+ * 你的算法只能使用常数的额外空间。
+ * 你不能只是单纯的改变节点内部的值 ，而是需要实际的进行节点交换。
+ * @Date 2019-05-26
+ * @Version 1.0
+ */
+public class LeetCode24 {
+
+
+    public static void main(String[] args) {
+
+    }
+}
