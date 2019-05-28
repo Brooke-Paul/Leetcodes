3c81e9b92f6df81409405ffed48395bc262b5189
xuetao <gentle.taoxue@gmail.com>
2019-05-28 23:37:26 +0800

    update leetcode25 swap linked inversion

--- src/main/java/com/learning/LeetCode25.java
+++ src/main/java/com/learning/LeetCode25.java
@@ -1,5 +1,9 @@
 package com.learning;
 
+import com.learning.entity.Node;
+
+import java.util.Objects;
+
 /**
  * @Author xuetao
  * @Description: 给出一个链表，每 k 个节点一组进行翻转，并返回翻转后的链表。
@@ -23,6 +27,40 @@ package com.learning;
  */
 public class LeetCode25 {
     public static void main(String[] args) {
-
+        Node node = new Node(Objects.hashCode(1), 1, 1, null);
+        node.next = new Node(Objects.hashCode(4), 4, 4, null);
+        node.next.next = new Node(Objects.hashCode(5), 5, 5, null);
+        node.next.next.next = new Node(Objects.hashCode(6), 6, 6, null);
+        node.next.next.next.next = new Node(Objects.hashCode(7), 7, 7, null);
+        Node result = reverseKGroup(node, 3);
+        while (result != null) {
+            System.out.println(result.value);
+            result = result.next;
+        }
     }
+
+    public static Node reverseKGroup(Node node, int k) {
+        Node curr = node;
+        int count = 0;
+        while (curr != null && count != k) {
+            curr = curr.next;
+            count++;
+        }
+
+
+
+        if (count == k) {
+            curr = reverseKGroup(curr, k);
+            while (count-- > 0) {
+                Node temp = node.next;
+                node.next = curr;
+                curr = node;
+                node = temp;
+            }
+            node = curr;
+        }
+
+        return node;
+    }
+
 }
