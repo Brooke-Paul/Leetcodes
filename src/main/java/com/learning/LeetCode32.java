0f5bb64b6ad071c705c7fa6a2435428109ddb310
xuetao <gentle.taoxue@gmail.com>
2019-06-05 01:10:23 +0800

    insert leetcode32 match () max length

---
+++ src/main/java/com/learning/LeetCode32.java
@@ -0,0 +1,30 @@
+package com.learning;
+
+/**
+ * @Author xuetao
+ * @Description: 给定一个只包含 '(' 和 ')' 的字符串，找出最长的包含有效括号的子串的长度。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: "(()"
+ * 输出: 2
+ * 解释: 最长有效括号子串为 "()"
+ * <p>
+ * 示例 2:
+ * <p>
+ * 输入: ")()())"
+ * 输出: 4
+ * 解释: 最长有效括号子串为 "()()"
+ * @Date 2019-06-05
+ * @Version 1.0
+ */
+public class LeetCode32 {
+    /**
+     * 利用map 判断 明日解决
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
