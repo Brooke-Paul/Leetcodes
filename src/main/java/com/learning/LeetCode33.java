ec2ec0d68e346e20c43b05f19e553812ca30eb07
xuetao <gentle.taoxue@gmail.com>
2019-06-07 21:51:47 +0800

    insert LeetCode33 findExistNum

--- src/main/java/com/learning/LeetCode33.java
+++ src/main/java/com/learning/LeetCode33.java
@@ -31,10 +31,36 @@ public class LeetCode33 {
      * @param args
      */
     public static void main(String[] args) {
-
+        int[] array = {4, 5, 6, 7, 0, 1, 2};
+        int target = 2;
+        System.out.println(findExistsNum(array, target));
     }
 
-    public static int findExistsNum() {
-
+    public static int findExistsNum(int[] array, int target) {
+        if (array == null || array.length < 1) {
+            return -1;
+        }
+        int left = 0;
+        int right = array.length;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (array[mid] == target) {
+                return mid;
+            }
+            if (array[mid] >= array[left]) {
+                if (array[left] <= target && target < array[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (array[mid] < target && target <= array[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return array[left] == target ? left : -1;
     }
 }
