79adf771782338d62670ecafd5c7edd606603ab5
xuetao <gentle.taoxue@gmail.com>
2019-06-08 22:38:09 +0800

    insert LeetCode34

---
+++ src/main/java/com/learning/LeetCode34.java
@@ -0,0 +1,18 @@
+package com.learning;
+
+/**
+ * @Author xuetao
+ * @Description: TODO
+ * @Date 2019-06-08
+ * @Version 1.0
+ */
+public class LeetCode34 {
+
+    /**
+     * 明日解决
+     */
+    public static void main(String[] args) {
+
+
+    }
+}
