01f5305cf80ad192772d775eb473f5e1883c0a51
xuetao <gentle.taoxue@gmail.com>
2019-06-15 23:30:18 +0800

    insert LeetCode39

---
+++ src/main/java/com/learning/LeetCode39.java
@@ -0,0 +1,38 @@
+package com.learning;
+
+/**
+ * @Author xuetao
+ * @Description: 给定一个 无重复元素 的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+ * <p>
+ * candidates 中的数字可以无限制重复被选取。
+ * <p>
+ * 说明：
+ * <p>
+ * 所有数字（包括 target ）都是正整数。
+ * 解集不能包含重复的组合。
+ * 示例 1:
+ * <p>
+ * 输入: candidates = [2,3,6,7], target = 7,
+ * 所求解集为:
+ * [
+ * [7],
+ * [2,2,3]
+ * ]
+ * <p>
+ * 示例 2:
+ * <p>
+ * 输入: candidates = [2,3,5], target = 8,
+ * 所求解集为:
+ * [
+ * [2,2,2,2],
+ * [2,3,3],
+ * [3,5]
+ * ]
+ * @Date 2019-06-15
+ * @Version 1.0
+ */
+public class LeetCode39 {
+    public static void main(String[] args) {
+
+    }
+}
