f5f4f3e6913e9e9e07b44e49df8ca3a36a01e26d
xuetao <gentle.taoxue@gmail.com>
2019-06-16 11:27:58 +0800

    update LeetCode39 Array combination

--- src/main/java/com/learning/LeetCode39.java
+++ src/main/java/com/learning/LeetCode39.java
@@ -1,5 +1,9 @@
 package com.learning;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @Author xuetao
  * @Description: 给定一个 无重复元素 的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
@@ -33,6 +37,40 @@ package com.learning;
  */
 public class LeetCode39 {
     public static void main(String[] args) {
+        int[] candidates = {2, 3, 6, 7};
+        int target = 7;
+        Arrays.sort(candidates);
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+        findChildCollection(list, new ArrayList(), candidates, target, 0);
+
+        list.forEach(i -> System.out.println(i));
+    }
+
+    /**
+     * 数字之和为target， 数字组合可重复，首先考虑递归操作，从第一位起循环嵌套，如果数组最小值大于target 去除最后一位继续遍历
+     *
+     * @param arrayList
+     * @param current
+     * @param array
+     * @param target
+     * @param start
+     */
+    public static void findChildCollection(List<List<Integer>> arrayList, List<Integer> current, int[] array, int target, int start) {
+        int length = array.length;
+        if (target > 0) {
+            for (int i = start; i < length; i++) {
+                if (array[i] > target) {
+                    break;
+                }
+                current.add(array[i]);
+                findChildCollection(arrayList, current, array, target - array[i], i);
+                current.remove(current.size() - 1);
+            }
+        } else if (target == 0) {
+            arrayList.add(new ArrayList<>(current));
+        }
 
     }
+
+
 }
