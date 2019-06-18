f6cc6a22281b629c786bf906201b5ea00e4b8d74
xuetao <gentle.taoxue@gmail.com>
2019-06-19 00:23:32 +0800

    insert LeetCode42 trapping rain water

---
+++ src/main/java/com/learning/LeetCode42.java
@@ -0,0 +1,30 @@
+package com.learning;
+
+/**
+ * @Author xuetao
+ * @Description: 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+ * <p>
+ * <p>
+ * <p>
+ * 上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。 感谢 Marcos 贡献此图。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [0,1,0,2,1,0,1,3,2,1,2,1]
+ * 输出: 6
+ * @Date 2019-06-19
+ * @Version 1.0
+ */
+public class LeetCode42 {
+
+    /**
+     * 明日解决
+     * 思考： 横坐标差与纵坐标差相乘
+     *
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
