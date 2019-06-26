5ac46ef2a723403eb495e78a3a2ee3f7d616ffdf
xuetao <gentle.taoxue@gmail.com>
2019-06-27 00:58:29 +0800

    insert LeetCode47 full permutation 2

---
+++ src/main/java/com/learning/LeetCode47.java
@@ -0,0 +1,29 @@
+package com.learning;
+
+/**
+ * @Author xuetao
+ * @Description: 给定一个可包含重复数字的序列，返回所有不重复的全排列。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [1,1,2]
+ * 输出:
+ * [
+ * [1,1,2],
+ * [1,2,1],
+ * [2,1,1]
+ * ]
+ * @Date 2019-06-27
+ * @Version 1.0
+ */
+public class LeetCode47 {
+
+    /**
+     * 思路：先排序，递归循环每次i+1
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
