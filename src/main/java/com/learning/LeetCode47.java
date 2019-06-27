1e3e3dc2fbf7a726727d89c6917ac8c488d19d64
xuetao <gentle.taoxue@gmail.com>
2019-06-28 02:10:32 +0800

    update LeetCode47 full permutation 2

--- src/main/java/com/learning/LeetCode47.java
+++ src/main/java/com/learning/LeetCode47.java
@@ -1,5 +1,7 @@
 package com.learning;
 
+import java.util.*;
+
 /**
  * @Author xuetao
  * @Description: 给定一个可包含重复数字的序列，返回所有不重复的全排列。
@@ -19,11 +21,58 @@ package com.learning;
 public class LeetCode47 {
 
     /**
-     * 思路：先排序，递归循环每次i+1
+     * 思路：先排序，通过map组装下标，保证下标不重复，然后组装List，最后判断集合数据
      *
      * @param args
      */
     public static void main(String[] args) {
+        int[] array = {1, 1, 2};
+        Arrays.sort(array);
+        fullPermutation(array);
+    }
+
+    public static void fullPermutation(int[] array) {
+        if (array == null || array.length < 1) {
+            return;
+        }
+        int length = array.length;
+        List<List<Integer>> lists = new ArrayList<>();
+        for (int i = 0; i < length; i++) {
+            Map map = new HashMap();
+            map.put(0, i);
+            findAllNext(lists, map, array, length);
+        }
+
+        lists.forEach(i -> {
+            i.forEach(j -> System.out.print(j));
+            System.out.println(" ");
+        });
 
     }
+
+    public static void findAllNext(List<List<Integer>> lists, Map map, int[] array, int length) {
+        if (map.size() == length) {
+            List list = new ArrayList();
+            Set<Map.Entry> set = map.entrySet();
+            Iterator<Map.Entry> iterator = set.iterator();
+            while (iterator.hasNext()) {
+                Map.Entry entry = iterator.next();
+                list.add(array[(Integer) entry.getValue()]);
+            }
+            if (!lists.contains(list)) {
+                lists.add(list);
+            }
+        } else {
+            for (int j = 0; j < length; j++) {
+                if (!map.containsValue(j)) {
+                    map.put(map.size(), j);
+                    findAllNext(lists, map, array, length);
+                    map.remove(map.size() - 1);
+                }
+
+            }
+        }
+
+    }
+
 }
