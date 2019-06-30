89ad8971b90e4b40b91e5ec8adc141ff58e681e4
xuetao <gentle.taoxue@gmail.com>
2019-06-30 17:40:42 +0800

    update leetCode 48 matrix

--- src/main/java/com/learning/LeetCode48.java
+++ src/main/java/com/learning/LeetCode48.java
@@ -31,14 +31,14 @@ package com.learning;
  * 给定 matrix =
  * [
  * [ 5, 1, 9,11],
- * [ 2, 4, 8,10],
- * [13, 3, 6, 7],
+ * [ 2, 4, 8,10],     3 6
+ * [13, 3, 6, 7],     4 8
  * [15,14,12,16]
  * ],
  * <p>
  * 原地旋转输入矩阵，使其变为:
  * [
- * [15,13, 2, 5],
+ * [15,13, 2, 5],    00-03 01-13 02-23 03 33
  * [14, 3, 4, 1],
  * [12, 6, 8, 9],
  * [16, 7,10,11]
@@ -48,6 +48,37 @@ package com.learning;
  */
 public class LeetCode48 {
     public static void main(String[] args) {
+        int[][] array = {{5, 1, 9, 11}, {2, 4, 8, 10}, {3, 3, 6, 7}, {15, 14, 12, 16}};
+        rotateMatrix(array);
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void rotateMatrix(int[][] array) {
+        int row = array.length;
+        int col = array[0].length;
+        int top = 0;
+        int bottom = row - 1;
+        while (top < bottom) {
+            int[] temp = array[top];
+            array[top] = array[bottom];
+            array[bottom] = temp;
+            top++;
+            bottom--;
+        }
+        for (int i = 0; i < row; i++) {
+            for (int j = i + 1; j < col; j++) {
+                int temp = array[i][j];
+                array[i][j] = array[j][i];
+                array[j][i] = temp;
+            }
+        }
 
     }
+
 }
