379f5e63422450475016f97a02b40ce10930a7f6
xuetao <gentle.taoxue@gmail.com>
2019-07-03 01:50:28 +0800

    update leetCode 51  solveNQueens

--- src/main/java/com/learning/LeetCode51.java
+++ src/main/java/com/learning/LeetCode51.java
@@ -1,5 +1,8 @@
 package com.learning;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Author xuetao
  * @Description: n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
@@ -33,6 +36,51 @@ package com.learning;
 public class LeetCode51 {
 
     public static void main(String[] args) {
+        List<List<String>> tempList = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        int n = 4;
+        solve(n, 0, tempList, list);
 
+        tempList.forEach(i -> System.out.println(i));
+    }
+
+
+    public static void solve(int n, int index, List<List<String>> tempList, List<Integer> list) {
+        if (n == index) {
+            List<String> tempStrList = new ArrayList<>();
+            for (int i = 0; i < list.size(); i++) {
+                int col = list.get(i);
+                StringBuffer sb = new StringBuffer();
+                for (int j = 0; j < n; j++) {
+                    if (j == col) {
+                        sb.append('Q');
+                    } else {
+                        sb.append('.');
+                    }
+                }
+                tempStrList.add(sb.toString());
+            }
+            tempList.add(tempStrList);
+            return;
+        }
+        for (int col = 0; col < n; col++) {
+            int row = index;
+            int rowList;
+            for (rowList = 0; rowList < list.size(); rowList++) {
+                int rowrow = rowList;
+                int colcol = list.get(rowrow);
+                if (col == colcol) {
+                    break;
+                }
+                if (Math.abs(rowrow - row) == Math.abs(colcol - col)) {
+                    break;
+                }
+            }
+            if (rowList == list.size()) {
+                list.add(col);
+                solve(n, index + 1, tempList, list);
+                list.remove(list.size() - 1);
+            }
+        }
     }
 }
