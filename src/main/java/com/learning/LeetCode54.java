53d7ee565b284d72c533701c9f6403d94f2015df
xuetao <gentle.taoxue@gmail.com>
2019-07-07 23:52:56 +0800

    update leetCode 54 spiral matrix

--- src/main/java/com/learning/LeetCode54.java
+++ src/main/java/com/learning/LeetCode54.java
@@ -1,11 +1,14 @@
 package com.learning;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Author xuetao
  * @Description: 给定一个包含 m x n 个元素的矩阵（ m 行, n 列），请按照顺时针螺旋顺序，返回矩阵中的所有元素。
- *
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 输入:
  * [
  * [ 1, 2, 3 ],
@@ -13,9 +16,9 @@ package com.learning;
  * [ 7, 8, 9 ]
  * ]
  * 输出: [1,2,3,6,9,8,7,4,5]
- *
+ * <p>
  * 示例 2:
- *
+ * <p>
  * 输入:
  * [
  * [1, 2, 3, 4],
@@ -27,4 +30,32 @@ package com.learning;
  * @Version 1.0
  */
 public class LeetCode54 {
+    public static void main(String[] args) {
+        int[][] array = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        spiralOrder(array);
+    }
+
+    public static List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        int row = matrix.length;
+
+
+        if (row == 0) {
+            return result;
+        }
+
+        int rowstart = 0;
+        int rowend = matrix.length - 1;
+
+        int colstart = 0;
+        int colend = matrix[0].length - 1;
+        while (rowstart <= rowend && colstart <= colend) {
+            for (int j = colstart; j <= colend; j++) {
+                result.add(matrix[rowstart][j]);
+            }
+        }
+        rowstart++;
+        return result;
+    }
+
 }
