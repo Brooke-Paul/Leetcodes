ab290874ea77f81fa401031a46b177cea2e13829
xuetao <gentle.taoxue@gmail.com>
2019-07-25 00:16:12 +0800

    insert leetCode 70 simplifypath

---
+++ src/main/java/com/learning/LeetCode70.java
@@ -0,0 +1,35 @@
+package com.learning;
+
+/**
+ * @Author xuetao
+ * @Description: 给定一个文档 (Unix-style) 的完全路径，请进行路径简化。
+ * <p>
+ * 例如，
+ * path = "/home/" , => "/home"
+ * path = "/a/./b/../../c/" , => "/c"
+ * <p>
+ * 边界情况:
+ * <p>
+ * 你是否考虑了 路径 = "/../" 的情况？
+ * 在这种情况下，你需返回 "/" 。
+ * 此外，路径中也可能包含多个斜杠 '/' ，如 "/home//foo/" 。
+ * 在这种情况下，你可忽略多余的斜杠，返回 "/home/foo" 。
+ * @Date 2019-07-25
+ * @Version 1.0
+ */
+public class LeetCode70 {
+    /**
+     * 思路： 去掉 .., ., // 中的 /
+     * 字符串分隔
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+    private static String simplifyPath(String path) {
+
+        return "";
+    }
+}
