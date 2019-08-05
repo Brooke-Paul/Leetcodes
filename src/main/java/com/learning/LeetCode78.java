cb1f3d6862a0000ebbd9e7922f0c41aa73b3eea9
xuetao <gentle.taoxue@gmail.com>
2019-08-05 23:06:08 +0800

    update leetCode 78 word search

--- src/main/java/com/learning/LeetCode78.java
+++ src/main/java/com/learning/LeetCode78.java
@@ -23,6 +23,52 @@ package com.learning;
  */
 public class LeetCode78 {
     public static void main(String[] args) {
-
+        char [][] chars = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
+        String target = "SEE";
+        System.out.println(exist(chars, target));
     }
+
+    private static boolean exist(char[][] board, String word) {
+        int m = board.length;
+        int n = board[0].length;
+        boolean[][] visited = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dfs(board, visited, i, j, word, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static final int[] dirs = new int[]{0, 1, 0, -1, 0};
+
+    private static boolean dfs(char[][] board, boolean[][] visited, int row, int col, String word, int index) {
+        if (index >= word.length() || word.charAt(index) != board[row][col]) {
+            return false;
+        } else if (index == word.length() - 1 && word.charAt(index) == board[row][col]) {
+            visited[row][col] = true;
+            return true;
+        }
+        //set it to true for this case
+        visited[row][col] = true;
+        boolean result = false;
+        for (int i = 0; i < 4; i++) {
+            int nextRow = row + dirs[i];
+            int nextCol = col + dirs[i + 1];
+            if (nextRow < 0 || nextRow >= board.length || nextCol < 0 || nextCol >= board[0].length || visited[nextRow][nextCol]) {
+                continue;
+            }
+            result = dfs(board, visited, nextRow, nextCol, word, index + 1);
+            if (result) {
+                return result;
+            } else {
+                //set it back to false if this road doesn't work to allow it for other paths, this is backtracking!!!
+                visited[nextRow][nextCol] = false;
+            }
+        }
+        return result;
+    }
+
 }
