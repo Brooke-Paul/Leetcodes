c7eafba80403684686494d23e3ce78c1fa9ddae1
xuetao <gentle.taoxue@gmail.com>
2019-08-10 22:16:05 +0800

    insert leetCode 82 remove duplicates one

---
+++ src/main/java/com/learning/LeetCode82.java
@@ -0,0 +1,23 @@
+package com.learning;
+
+/**
+ * @Author xuetao
+ * @Description: Given a sorted linked list, delete all duplicates such that each element appear only once.
+ * <p>
+ * For example,
+ * Given 1->1->2, return 1->2.
+ * Given 1->1->2->3->3, return 1->2->3.
+ * ---------------------
+ * @Date 2019-08-10
+ * @Version 1.0
+ */
+public class LeetCode82 {
+    /**
+     * 上一题简便做法，明日解决
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
