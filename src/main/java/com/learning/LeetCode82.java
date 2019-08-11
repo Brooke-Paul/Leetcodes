1c942436ff59b30193b93afa3033529402045db1
xuetao <gentle.taoxue@gmail.com>
2019-08-11 21:27:01 +0800

    update leetCode 82 remove duplicates one

--- src/main/java/com/learning/LeetCode82.java
+++ src/main/java/com/learning/LeetCode82.java
@@ -1,5 +1,7 @@
 package com.learning;
 
+import com.learning.entity.Node;
+
 /**
  * @Author xuetao
  * @Description: Given a sorted linked list, delete all duplicates such that each element appear only once.
@@ -18,6 +20,39 @@ public class LeetCode82 {
      * @param args
      */
     public static void main(String[] args) {
+
+        Node node = new Node(1, 1, 1, null);
+        node.next = new Node(1, 1, 1, null);
+        node.next.next = new Node(2, 2, 2, null);
+        node.next.next.next = new Node(3, 3, 3, null);
+        node.next.next.next.next = new Node(3, 3, 3, null);
+        removeDuplicates(node);
+    }
+
+    private static void removeDuplicates(Node node) {
+        Node first = new Node(0, 0, 0, null);
+        Node firstTemp = first;
+        Node temp = node;
+
+
+        int value = 0;
+        while (temp != null) {
+            if ((int) temp.value != value) {
+                firstTemp.next = temp;
+                firstTemp = firstTemp.next;
+            } else {
+                firstTemp.next = null;
+            }
+            value = (int) temp.value;
+            temp = temp.next;
+
+        }
+        node = first.next;
+        while (node != null) {
+            System.out.println(node.value);
+            node = node.next;
+
+        }
 
     }
 }
