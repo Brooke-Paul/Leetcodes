f576720759d01d1f45cf47627d023f26b11de193
xuetao <gentle.taoxue@gmail.com>
2019-09-18 00:08:11 +0800

    insert leetCode117 numerator/denominator

---
+++ src/main/java/com/learning/Number150/LeetCode117.java
@@ -0,0 +1,35 @@
+package com.learning.Number150;
+
+/**
+ * @Author xuetao
+ * @Description: 给定两个整数，分别表示分数的分子 numerator 和分母 denominator，以字符串形式返回小数。
+ * <p>
+ * 如果小数部分为循环小数，则将循环的部分括在括号内。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: numerator = 1, denominator = 2
+ * 输出: "0.5"
+ * <p>
+ * 示例 2:
+ * <p>
+ * 输入: numerator = 2, denominator = 1
+ * 输出: "2"
+ * <p>
+ * 示例 3:
+ * <p>
+ * 输入: numerator = 2, denominator = 3
+ * 输出: "0.(6)"
+ * @Date 2019-09-18
+ * @Version 1.0
+ */
+public class LeetCode117 {
+    /**
+     * 明日解决
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
