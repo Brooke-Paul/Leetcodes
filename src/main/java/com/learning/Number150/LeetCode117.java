d2cbbe7d4a05c7f247b4980e74317c67cf8f6b72
xuetao <gentle.taoxue@gmail.com>
2019-09-18 19:23:57 +0800

    update leetCode117 numerator/denominator

--- src/main/java/com/learning/Number150/LeetCode117.java
+++ src/main/java/com/learning/Number150/LeetCode117.java
@@ -1,5 +1,8 @@
 package com.learning.Number150;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Author xuetao
  * @Description: 给定两个整数，分别表示分数的分子 numerator 和分母 denominator，以字符串形式返回小数。
@@ -25,11 +28,41 @@ package com.learning.Number150;
  */
 public class LeetCode117 {
     /**
-     * 明日解决
      *
      * @param args
      */
     public static void main(String[] args) {
+        System.out.println(fractionToDecimal(2, 3));
+    }
 
+    public static String fractionToDecimal(int numerator, int denominator) {
+        if (numerator == 0) {
+            return "0";
+        }
+        StringBuilder sb = new StringBuilder();
+        if (numerator < 0 ^ denominator < 0) {
+            sb.append("-");
+        }
+        long a = Math.abs(Long.valueOf(numerator));
+        long b = Math.abs(Long.valueOf(denominator));
+        long mod = a % b;
+        sb.append(String.valueOf(a / b));
+        if (mod == 0) {
+            return sb.toString();
+        }
+        sb.append(".");
+        Map<Long, Integer> map = new HashMap<>();
+        while (mod != 0) {
+            if (map.containsKey(mod)) {
+                sb.insert(map.get(mod), "(");
+                sb.append(")");
+                break;
+            }
+            map.put(mod, sb.length());
+            mod *= 10;
+            sb.append(String.valueOf(mod / b));
+            mod %= b;
+        }
+        return sb.toString();
     }
 }
