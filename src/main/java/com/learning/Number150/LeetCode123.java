4c258fa9ddaad8861943eec3e503bc094e662dac
xuetao <gentle.taoxue@gmail.com>
2019-09-26 00:01:56 +0800

    update leetcode 123 find biggest number

--- src/main/java/com/learning/Number150/LeetCode123.java
+++ src/main/java/com/learning/Number150/LeetCode123.java
@@ -1,5 +1,8 @@
 package com.learning.Number150;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * @Author xuetao
  * @Description: 给定一组非负整数，重新排列它们的顺序使之组成一个最大的整数。
@@ -25,6 +28,33 @@ public class LeetCode123 {
      * @param args
      */
     public static void main(String[] args) {
+        int[] array = {3, 30, 34, 5, 9};
+        System.out.println(largestNumber(array));
+    }
 
+    /**
+     * 思路：构造一个比较器，用字符串去做比较，大的放在前面
+     * @param nums
+     * @return
+     */
+    public static String largestNumber(int[] nums) {
+        String[] str = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            str[i] = String.valueOf(nums[i]);
+        }
+        Comparator<String> comp = new Comparator<String>() {
+            public int compare(String o1, String o2) {
+                return (o2 + o1).compareTo(o1 + o2);
+            }
+        };
+        Arrays.sort(str, comp);
+        if (str[0].equals("0")) {
+            return "0";
+        }
+        StringBuilder sb = new StringBuilder();
+        for (String s : str) {
+            sb.append(s);
+        }
+        return sb.toString();
     }
 }
