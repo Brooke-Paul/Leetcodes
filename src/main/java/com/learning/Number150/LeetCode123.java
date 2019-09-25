a5d57e25a733efcad34ec18459508e856e0beea6
xuetao <gentle.taoxue@gmail.com>
2019-09-25 22:22:51 +0800

    insert leetcode 123 find biggest number

---
+++ src/main/java/com/learning/Number150/LeetCode123.java
@@ -0,0 +1,30 @@
+package com.learning.Number150;
+
+/**
+ * @Author xuetao
+ * @Description: 给定一组非负整数，重新排列它们的顺序使之组成一个最大的整数。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [10,2]
+ * 输出: 210
+ * <p>
+ * 示例 2:
+ * <p>
+ * 输入: [3,30,34,5,9]
+ * 输出: 9534330
+ * <p>
+ * 说明: 输出结果可能非常大，所以你需要返回一个字符串而不是整数
+ * @Date 2019-09-25
+ * @Version 1.0
+ */
+public class LeetCode123 {
+    /**
+     * 获取最大整数
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
