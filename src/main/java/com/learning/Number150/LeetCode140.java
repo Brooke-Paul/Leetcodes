60971f1c1b5fa820cf4c0a0c4822c6270fe22f05
薛滔 <gentle.taoxue@gmail.com>
2019-10-15 23:15:15 +0800

    insert LeetCode140

---
+++ src/main/java/com/learning/Number150/LeetCode140.java
@@ -0,0 +1,14 @@
+package com.learning.Number150;
+
+/**
+ * @Author xuetao
+ * @Description: TODO
+ * @Date 2019-10-15
+ * @Version 1.0
+ */
+public class LeetCode140 {
+    public static void main(String[] args) {
+
+    }
+
+}
