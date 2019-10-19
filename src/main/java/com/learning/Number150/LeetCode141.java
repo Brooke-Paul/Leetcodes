f979ca635022b60353101d1fd957a12846490809
薛滔 <gentle.taoxue@gmail.com>
2019-10-19 22:29:06 +0800

    insert LeetCode141 rob

--- src/main/java/com/learning/Number150/LeetCode141.java
+++ src/main/java/com/learning/Number150/LeetCode141.java
@@ -1,33 +1,63 @@
 package com.learning.Number150;
 
+
 /**
  * @Author xuetao
- * @Description: 设计一个支持以下两种操作的数据结构：
- *
- * void addWord(word)
- * bool search(word)
- *
- * search(word) 可以搜索文字或正则表达式字符串，字符串只包含字母 . 或 a-z 。 . 可以表示任何一个字母。
- *
- * 示例:
- *
- * addWord("bad")
- * addWord("dad")
- * addWord("mad")
- * search("pad") -> false
- * search("bad") -> true
- * search(".ad") -> true
- * search("b..") -> true
- *
- * 说明:
- *
- * 你可以假设所有单词都是由小写字母 a-z 组成的
+ * @Description: 你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。这个地方所有的房屋都 围成一圈， 这意味着第一个房屋和最后一个房屋是紧挨着的。同时，相邻的房屋装有相互连通的防盗系统， 如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警 。
+ * <p>
+ * 给定一个代表每个房屋存放金额的非负整数数组，计算你 在不触动警报装置的情况下， 能够偷窃到的最高金额。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [2,3,2]
+ * 输出: 3
+ * 解释: 你不能先偷窃 1 号房屋（金额 = 2），然后偷窃 3 号房屋（金额 = 2）, 因为他们是相邻的。
+ * <p>
+ * 示例 2:
+ * <p>
+ * 输入: [1,2,3,1]
+ * 输出: 4
+ * 解释: 你可以先偷窃 1 号房屋（金额 = 1），然后偷窃 3 号房屋（金额 = 3）。
+ * 偷窃到的最高金额 = 1 + 3 = 4 。
  * @Date 2019-10-16
  * @Version 1.0
  */
 public class LeetCode141 {
 
     public static void main(String[] args) {
-
+        int[] nums = {1, 2, 3, 1};
+        System.out.println(rob(nums));
     }
+
+    public static int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        if (nums.length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        int[] dp = new int[nums.length - 1];
+
+        //rob 1 to n-1
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length - 1; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        int prevMax = dp[nums.length - 2];
+
+        //rob 2 to n
+        dp = new int[nums.length - 1];
+        dp[0] = nums[1];
+        dp[1] = Math.max(nums[1], nums[2]);
+        for (int i = 3; i < nums.length; i++) {
+            dp[i - 1] = Math.max(dp[i - 3] + nums[i], dp[i - 2]);
+        }
+        return Math.max(prevMax, dp[nums.length - 2]);
+    }
+
 }
+
