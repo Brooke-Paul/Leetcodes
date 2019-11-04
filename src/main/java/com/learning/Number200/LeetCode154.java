d2c952e2009bbe859d2c7ab917e2319b19be0e0f
薛滔 <gentle.taoxue@gmail.com>
2019-11-05 00:37:17 +0800

    update leetCode153 power of two

--- src/main/java/com/learning/Number200/LeetCode154.java
+++ src/main/java/com/learning/Number200/LeetCode154.java
@@ -23,8 +23,29 @@ package com.learning.Number200;
  */
 public class LeetCode154 {
     public static void main(String[] args) {
-
+        System.out.println(powerOfTwo(1));
     }
 
+    private static boolean powerOfTwo(int num) {
+        int count = 0;
+
+        if (num == 0) {
+            return false;
+        }
+        if (num == 1) {
+            return true;
+        }
+        if (num % 2 != 0) {
+            return false;
+        }
+
+
+        while (num % 2 == 0) {
+            count++;
+            num = num >> 1;
+        }
+
+        return count % 2 == 0;
+    }
 
 }
