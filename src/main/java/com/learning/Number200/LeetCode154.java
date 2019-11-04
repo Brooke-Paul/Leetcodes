fe633c10d60554b9d885e30c504f126813b40eba
薛滔 <gentle.taoxue@gmail.com>
2019-11-04 22:51:12 +0800

    insert leetCode153 power of two

---
+++ src/main/java/com/learning/Number200/LeetCode154.java
@@ -0,0 +1,30 @@
+package com.learning.Number200;
+
+/**
+ * @Author xuetao
+ * @Description: 给定一个整数，写一个函数来判断它是否是 2 的幂次方。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: 1
+ * 输出: true
+ * <p>
+ * 示例 2:
+ * <p>
+ * 输入: 16
+ * 输出: true
+ * <p>
+ * 示例 3:
+ * <p>
+ * 输入: 218
+ * 输出: false
+ * @Date 2019-11-04
+ * @Version 1.0
+ */
+public class LeetCode154 {
+    public static void main(String[] args) {
+
+    }
+
+
+}
