400d16ff3bf777136e8e42afb31bff89681df377
薛滔 <gentle.taoxue@gmail.com>
2019-11-09 23:11:33 +0800

    update leetCode159 searches matrix

--- src/main/java/com/learning/Number200/LeetCode159.java
+++ src/main/java/com/learning/Number200/LeetCode159.java
@@ -26,6 +26,31 @@ package com.learning.Number200;
  */
 public class LeetCode159 {
     public static void main(String[] args) {
+        int[][] array = {{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}};
+        System.out.println(findTarget(array, 25));
+    }
+
+    private static boolean findTarget(int[][] array, int target) {
+
+        int row = array.length;
+        int col = array[0].length;
+        for (int i = 0; i < row; i++) {
+            if (array[i][0] <= target && array[i][row - 1] >= target) {
+                int left = 0;
+                int right = col;
+                while (left < right) {
+                    int mid = (left + right) / 2;
+                    if (array[i][mid] < target) {
+                        left++;
+                    } else if (array[i][mid] > target) {
+                        right--;
+                    } else {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
 
     }
 }
