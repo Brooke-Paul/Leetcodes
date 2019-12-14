c149e7a131a911521b473fa6e39a6343067f5b17
薛滔 <gentle.taoxue@gmail.com>
2019-12-14 22:51:12 +0800

    insert leetCode187 coinChange

---
+++ src/main/java/com/learning/Number200/LeetCode187.java
@@ -0,0 +1,50 @@
+package com.learning.Number200;
+
+import java.util.Arrays;
+
+/**
+ * Program Name: leetcodes
+ * <p>
+ * Description:给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1 。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: coins = [1, 2, 5], amount = 11
+ * 输出: 3
+ * 解释: 11 = 5 + 5 + 1
+ * <p>
+ * 示例 2:
+ * <p>
+ * 输入: coins = [2], amount = 3
+ * 输出: -1
+ * <p>
+ * 说明 :
+ * 你可以认为每种硬币的数量是无限的。
+ * <p>
+ * Created by xuetao on 2019/12/14
+ *
+ * @author xuetao
+ * @version 1.0
+ */
+public class LeetCode187 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 5};
+        int amount = 11;
+        System.out.println(coinChange(array, amount));
+    }
+
+    private static int coinChange(int[] coins, int amount) {
+        int max = amount + 1;
+        int[] dp = new int[max];
+        Arrays.fill(dp, max);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
