bac12c15fa2c472198a78d4785dfaf56d83725dc
薛滔 <gentle.taoxue@gmail.com>
2019-12-20 23:03:21 +0800

    insert leetCode191 findItinerary

---
+++ src/main/java/com/learning/Number200/LeetCode191.java
@@ -0,0 +1,29 @@
+package com.learning.Number200;
+
+/**
+ * Program Name: leetcodes
+ * <p>
+ * Description: 给定一个机票的字符串二维数组 [from, to] ，子数组中的两个成员分别表示飞机出发和降落的机场地点，对该行程进行重新规划排序。所有这些机票都属于一个从JFK（肯尼迪国际机场）出发的先生，所以该行程必须从 JFK 出发。
+ * <p>
+ * 说明:
+ * <p>
+ * 如果存在多种有效的行程，你可以按字符自然排序返回最小的行程组合。例如，行程 ["JFK", "LGA"] 与 ["JFK", "LGB"] 相比就更小，排序更靠前
+ * 所有的机场都用三个大写字母表示（机场代码）。
+ * 假定所有机票至少存在一种合理的行程。
+ * 示例 1:
+ * <p>
+ * 输入:
+ * 示例 2:
+ * <p>
+ * 输入:
+ * <p>
+ * Created by xuetao on 2019/12/20
+ *
+ * @author xuetao
+ * @version 1.0
+ */
+public class LeetCode191 {
+    public static void main(String[] args) {
+
+    }
+}
