d701a684bdba2c2b858b56dc317bebc4bc68444f
薛滔 <gentle.taoxue@gmail.com>
2019-12-24 23:01:12 +0800

    insert leetCode195 iterator

---
+++ src/main/java/com/learning/Number200/LeetCode195.java
@@ -0,0 +1,29 @@
+package com.learning.Number200;
+
+/**
+ * Program Name: leetcodes
+ * <p>
+ * Description: 给出一个嵌套的整型列表。设计一个迭代器，遍历这个整型列表中的所有整数。
+ *
+ * 列表中的项或者为一个整数，或者是另一个列表。
+ *
+ * 示例 1:
+ * 给定列表 [[1,1],2,[1,1]] ,
+ *
+ * 通过重复调用 next 直到 hasNex t 返回false， next 返回的元素的顺序应该是: [1,1,2,1,1] .
+ *
+ * 示例 2:
+ * 给定列表 [1,[4,[6]]] ,
+ *
+ * 通过重复调用 next 直到 hasNex t 返回false， next 返回的元素的顺序应该是: [1,4,6] .
+ * <p>
+ * Created by xuetao on 2019/12/24
+ *
+ * @author xuetao
+ * @version 1.0
+ */
+public class LeetCode195 {
+    public static void main(String[] args) {
+
+    }
+}
