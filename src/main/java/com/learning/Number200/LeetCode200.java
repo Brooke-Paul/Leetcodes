3d1643694fc888ad88a45c2bb85b03c566271f1e
薛滔 <gentle.taoxue@gmail.com>
2019-12-30 21:42:20 +0800

    insert leetCode200 topK

---
+++ src/main/java/com/learning/Number200/LeetCode200.java
@@ -0,0 +1,26 @@
+package com.learning.Number200;
+
+/**
+ * Program Name: leetcodes
+ * <p>
+ * Description: 给定一个非空的整数数组，返回其中出现频率前 k 高的元素。
+ * <p>
+ * 例如，
+ * <p>
+ * 给定数组 [1,1,1,2,2,3] , 和 k = 2，返回 [1,2] 。
+ * <p>
+ * 注意：
+ * <p>
+ * 你可以假设给定的 k 总是合理的，1 ≤ k ≤ 数组中不相同的元素的个数。
+ * 你的算法的时间复杂度 必须 优于 O( n log n ) , n 是数组的大小。
+ * <p>
+ * Created by xuetao on 2019/12/30
+ *
+ * @author xuetao
+ * @version 1.0
+ */
+public class LeetCode200 {
+    public static void main(String[] args) {
+
+    }
+}
