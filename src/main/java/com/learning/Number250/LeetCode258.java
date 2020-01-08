8b9c84be61a150a99529e105b1cb559d16e9341e
薛滔 <gentle.taoxue@gmail.com>
2020-01-08 20:54:36 +0800

    insert leetCode258 superPow

---
+++ src/main/java/com/learning/Number250/LeetCode258.java
@@ -0,0 +1,39 @@
+package com.learning.Number250;
+
+/**
+ * Program Name: leetcodes
+ * <p>
+ * Description: 你的任务是计算 a b 对 1337 取模， a 是一个正整数， b 是一个非常大的正整数且会以数组形式给出。
+ * <p>
+ * 示例 1:
+ * <p>
+ * a = 2
+ * b = [3]
+ * <p>
+ * 结果: 8
+ * <p>
+ * 示例 2:
+ * <p>
+ * a = 2
+ * b = [1,0]
+ * <p>
+ * 结果: 1024
+ * <p>
+ * 致谢：
+ * <p>
+ * 特别感谢 @Stomach_ache 添加这道题并创建所有测试用例。
+ * <p>
+ * Created by xuetao on 2020/1/8
+ *
+ * @author xuetao
+ * @version 1.0
+ */
+public class LeetCode258 {
+    public static void main(String[] args) {
+
+    }
+
+    private static int superPow() {
+
+    }
+}
