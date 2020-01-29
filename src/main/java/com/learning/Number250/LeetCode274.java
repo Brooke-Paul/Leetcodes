50e7717117619e78b7897d584513a2d595d34074
薛滔 <gentle.taoxue@gmail.com>
2020-01-29 22:55:39 +0800

    insert leetCode274 find longest subsequence

---
+++ src/main/java/com/learning/Number250/LeetCode274.java
@@ -0,0 +1,34 @@
+package com.learning.Number250;
+
+/**
+ * Program Name: leetcodes
+ * <p>
+ * Description: 找到给定字符串（由小写字符组成）中的最长子串 T ， 要求 T 中的每一字符出现次数都不少于 k 。输出 T 的长度。
+ *
+ * 示例 1:
+ *
+ * 输入:
+ * s = "aaabb", k = 3
+ *
+ * 输出:
+ * 3
+ *
+ * 最长子串为 "aaa" ，其中 'a' 重复了 3 次。
+ *
+ * 示例 2:
+ *
+ * 输入:
+ * s = "ababbc", k = 2
+ *
+ * 输出:
+ * 5
+ *
+ * 最长子串为 "ababb" ，其中 'a' 重复了 2 次， 'b' 重复了 3 次。
+ * <p>
+ * Created by xuetao on 2020/1/29
+ *
+ * @author xuetao
+ * @version 1.0
+ */
+public class LeetCode274 {
+}
