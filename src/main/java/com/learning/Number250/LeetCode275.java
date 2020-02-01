0c5cba0e3a77be4ce47e3757c679b4d00daab573
薛滔 <gentle.taoxue@gmail.com>
2020-02-01 23:38:32 +0800

    update leetCode275 maxRotateFunction

--- src/main/java/com/learning/Number250/LeetCode275.java
+++ src/main/java/com/learning/Number250/LeetCode275.java
@@ -32,6 +32,24 @@ package com.learning.Number250;
  */
 public class LeetCode275 {
     public static void main(String[] args) {
+        int[] A = {4, 3, 2, 6};
+        System.out.println(maxRotateFunction(A));
+    }
 
+    public static int maxRotateFunction(int[] A) {
+        int allSum = 0;
+        int len = A.length;
+        int F = 0;
+        for (int i = 0; i < len; i++) {
+            F += i * A[i];
+            allSum += A[i];
+        }
+        int max = F;
+        for (int i = len - 1; i >= 1; i--) {
+            F = F + allSum - len * A[i];
+            System.out.println(F);
+            max = Math.max(F, max);
+        }
+        return max;
     }
 }
