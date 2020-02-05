fcd048ccf109c5fbe0d07eaf4ed39b8a3008a861
薛滔 <gentle.taoxue@gmail.com>
2020-02-05 23:37:11 +0800

    insert leetCode277 randomInt

---
+++ src/main/java/com/learning/Number250/LeetCode277.java
@@ -0,0 +1,31 @@
+package com.learning.Number250;
+
+/**
+ * Program Name: leetcodes
+ * <p>
+ * Description: 给定一个可能含有重复元素的整数数组，要求随机输出给定的数字的索引。 您可以假设给定的数字一定存在于数组中。
+ * <p>
+ * 注意：
+ * 数组大小可能非常大。 使用太多额外空间的解决方案将不会通过测试。
+ * <p>
+ * 示例:
+ * <p>
+ * int[] nums = new int[] {1,2,3,3,3};
+ * Solution solution = new Solution(nums);
+ * <p>
+ * // pick(3) 应该返回索引 2,3 或者 4。每个索引的返回概率应该相等。
+ * solution.pick(3);
+ * <p>
+ * // pick(1) 应该返回 0。因为只有nums[0]等于1。
+ * solution.pick(1)
+ * <p>
+ * Created by xuetao on 2020/2/5
+ *
+ * @author xuetao
+ * @version 1.0
+ */
+public class LeetCode277 {
+    public static void main(String[] args) {
+
+    }
+}
