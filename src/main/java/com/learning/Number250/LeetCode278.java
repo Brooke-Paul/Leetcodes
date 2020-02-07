ce6ce61efcd1e4cb97e9d86333c9fb215f603c0d
薛滔 <gentle.taoxue@gmail.com>
2020-02-07 23:28:33 +0800

    insert leetCode278

---
+++ src/main/java/com/learning/Number250/LeetCode278.java
@@ -0,0 +1,18 @@
+package com.learning.Number250;
+
+/**
+ * Program Name: leetcodes
+ * <p>
+ * Description:
+ * <p>
+ * Created by xuetao on 2020/2/7
+ *
+ * @author xuetao
+ * @version 1.0
+ */
+public class LeetCode278 {
+    public static void main(String[] args) {
+
+    }
+
+}
