4a313faee91b2c1f885aeb6d31ba67c21076188b
薛滔 <gentle.taoxue@gmail.com>
2020-02-10 23:45:09 +0800

    add TreeNode

---
+++ src/main/java/com/learning/entity/TreeNode.java
@@ -0,0 +1,45 @@
+package com.learning.entity;
+
+/**
+ * Program Name: leetcodes
+ * <p>
+ * Description:
+ * <p>
+ * Created by xuetao on 2020/2/10
+ *
+ * @author xuetao
+ * @version 1.0
+ */
+public class TreeNode {
+    TreeNode left;
+    TreeNode right;
+    Integer val;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+
+    public Integer getVal() {
+        return val;
+    }
+
+    public void setVal(Integer val) {
+        this.val = val;
+    }
+}
