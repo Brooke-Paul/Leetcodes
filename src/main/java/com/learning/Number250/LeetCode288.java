40fe05db9deb048416a35b9eb74dd32821f7fff8
薛滔 <gentle.taoxue@gmail.com>
2020-02-21 22:56:16 +0800

    insert LeetCode288 canPartition

---
+++ src/main/java/com/learning/Number250/LeetCode288.java
@@ -0,0 +1,79 @@
+package com.learning.Number250;
+
+import java.util.Arrays;
+
+/**
+ * Program Name: com.learning.Number250
+ * Description: 给定一个 只包含正整数 的 非空 数组。是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
+ * <p>
+ * 注意:
+ * <p>
+ * 每个数组中的元素不会超过 100
+ * 数组的大小不会超过 200
+ * 示例 1:
+ * <p>
+ * 输入: [1, 5, 11, 5]
+ * <p>
+ * 输出: true
+ * <p>
+ * 解释: 数组可以分割成 [1, 5, 5] 和 [11].
+ * <p>
+ * 示例 2:
+ * <p>
+ * 输入: [1, 2, 3, 5]
+ * <p>
+ * 输出: false
+ * <p>
+ * 解释: 数组不能分割成两个元素和相等的子集.
+ * Created by xuetao on 2020/2/21
+ *
+ * @author xuetao
+ * @version 1.0
+ */
+
+public class LeetCode288 {
+    public static void main(String[] args) {
+        int[] array = {1, 5, 11, 5};
+        System.out.println(canPartition(array));
+    }
+
+    public static boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+
+        if ((sum & 1) == 1) {
+            return false;
+        }
+
+        sum /= 2;
+
+        int n = nums.length;
+        boolean[][] dp = new boolean[n + 1][sum + 1];
+        for (int i = 0; i < dp.length; i++) {
+            Arrays.fill(dp[i], false);
+        }
+
+        dp[0][0] = true;
+
+        for (int i = 1; i < n + 1; i++) {
+            dp[i][0] = true;
+        }
+
+        for (int j = 1; j < sum + 1; j++) {
+            dp[0][j] = false;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < sum + 1; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j >= nums[i - 1]) {
+                    dp[i][j] = (dp[i][j] || dp[i - 1][j - nums[i - 1]]);
+                }
+            }
+        }
+
+        return dp[n][sum];
+    }
+}
